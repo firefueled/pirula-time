675997d9dd10214bd510857cb90ae9a9ff0bd9ac
pablo.mint <slpkvol3@gmail.com>
2017-06-05 23:17:52 -0300

    Add cron job to trigger cloud function

--- webapp/app.rb
+++ webapp/app.rb
@@ -1,15 +1,19 @@
+require 'uri'
 require "sinatra"
+require 'net/http'
 require "google/cloud/datastore"
 
-before do
-  @@project_id = "pirula-time"
-end
+project_id = "pirula-time"
+datastore = Google::Cloud::Datastore.new project: project_id
+scrapeUri = URI("https://us-central1-pirula-time.cloudfunctions.net/doIt")
 
 get "/" do
-  datastore = Google::Cloud::Datastore.new project: @@project_id
-
   average = datastore.find('time', 'average')['val']
-  average = Time.at(average).strftime "%M:%S"
+  average = [average/60, average%60]
 
-  "O tempo médio do Pirula é #{average}"
+  "1 unidade de tempo Pirula equivale a #{average[0]} minutos e #{average[1]} segundos"
+end
+
+get "/runScrape" do
+  Net::HTTP.get(scrapeUri)
 end
