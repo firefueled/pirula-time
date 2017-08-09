258a7c9114ecf83a7512e5ff456f2432ea08188f
pablo.mint <slpkvol3@gmail.com>
2017-08-08 22:59:35 -0300

    Support for more data on the latestVids

--- webapp/application.py
+++ webapp/application.py
@@ -13,82 +13,77 @@ dataTb = dyndb.Table('Data')
 dataKey = boto3.dynamodb.conditions.Key('id').eq(0)
 
 def retrieveLatest():
-    data = {'latestDislikes': [], 'latestLikes': []}
+    data = { 'latestVideos': [] }
     res = dataTb.query(Limit=1,ScanIndexForward=False,KeyConditionExpression=dataKey)
     if len(res['Items']) == 0:
         return None
 
     res = res['Items'][0]
-    for item in res['latestDislikes'].split(';'):
-        if math.isnan(float(item)):
-            data['latestDislikes'].append(-1)
+
+    for item in res['latestVideos']:
+        item['id'] = int(item['id'])
+        if item['quality']:
+            item['imageSrc'] = '/static/images/pirula-yes.png'
         else:
-            data['latestDislikes'].append(int(item))
-    for item in res['latestLikes'].split(';'):
-        if math.isnan(float(item)):
-            data['latestLikes'].append(-1)
-        else:
-            data['latestLikes'].append(int(item))
+            item['imageSrc'] = '/static/images/pirula-no.png'
+        data['latestVideos'].append(item)
+
+
     data['averageDuration'] = int(res['averageDuration'])
     data['latestDuration'] = int(res['latestDuration'])
+    data['latestHate'] = int(res['latestHate'])
     return data
 
+def processData(data):
+    avgMin, avgSec = data['averageDuration']/60, data['averageDuration']%60
+    data['averageDurationMin'] = avgMin
+    data['averageDurationSec'] = avgSec
+
+    latestPirulaDuration = float(data['latestDuration'])/data['averageDuration']
+    data['latestPirulaDuration'] = '{:.2}'.format(latestPirulaDuration)
+
+    if latestPirulaDuration >= 1.5:
+        data['latestDurationSubjective'] = u'Hmmm... Delícia!'
+    elif latestPirulaDuration >= 1:
+        data['latestDurationSubjective'] = u'Bacana!'
+    elif latestPirulaDuration >= 0.5:
+        data['latestDurationSubjective'] = u'Meero...'
+    elif latestPirulaDuration < 0.5:
+        if latestPirulaDuration >= 0.4:
+            data['latestDurationSubjective'] = u'Vamo comer salgadinho?'
+        data['latestDurationSubjective'] = u'O que é isso, Ricardo!?...'
+    elif latestPirulaDuration <= 0.3:
+        data['latestDurationSubjective'] = u'Ai que Burro. Dá zero pra ele'
+
+    latestHate = data['latestHate']
+    if latestHate == -1:
+        data['latestHate'] = u'???'
+        data['latestHateSubjective'] = u'IIIhh Deu pra trás...'
+    else:
+        if latestHate >= 25000:
+            data['latestHateSubjective'] = u'Tá bom! Sou evangélico agora.'
+        elif latestHate >= 15000:
+            data['latestHateSubjective'] = u'OK OK! A terra é plana. Satifeitos?'
+        elif latestHate >= 7000:
+            data['latestHateSubjective'] = u'Se segura que lá vem chumbo!'
+        elif latestHate >= 4500:
+            data['latestHateSubjective'] = u'Ai meu Senhor Jesus...'
+        elif latestHate >= 3000:
+            data['latestHateSubjective'] = u'O canal é meu, viu!!?'
+        elif latestHate >= 1000:
+            data['latestHateSubjective'] = u'Haters gonna hate...'
+        elif latestHate > 500:
+            data['latestHateSubjective'] = u'Acho que ouvi algum zunido...'
+        elif latestHate <= 500:
+            data['latestHateSubjective'] = u'Nem faz cócegas...'
+
+    return data
 
 def getData():
     data = retrieveLatest()
 
     if (data != None):
-        avgMin, avgSec = data['averageDuration']/60, data['averageDuration']%60
-        data['averageDurationMin'] = avgMin
-        data['averageDurationSec'] = avgSec
-
-        latestPirulaDuration = float(data['latestDuration'])/data['averageDuration']
-        data['latestPirulaDuration'] = '{:.2}'.format(latestPirulaDuration)
-
-        if latestPirulaDuration >= 1.5:
-            data['latestDurationSubjective'] = u'Hmmm... Delícia!'
-        elif latestPirulaDuration >= 1:
-            data['latestDurationSubjective'] = u'Bacana!'
-        elif latestPirulaDuration >= 0.5:
-            data['latestDurationSubjective'] = u'Meero...'
-        elif latestPirulaDuration < 0.5:
-            if latestPirulaDuration >= 0.4:
-                data['latestDurationSubjective'] = u'Vamo comer salgadinho?'
-            data['latestDurationSubjective'] = u'O que é isso, Ricardo!?...'
-        elif latestPirulaDuration <= 0.3:
-            data['latestDurationSubjective'] = u'Ai que Burro. Dá zero pra ele'
-
-        latestHate = data['latestDislikes'][0]
-        if latestHate == -1:
-            data['latestHate'] = u'???'
-            data['latestHateSubjective'] = u'IIIhh Deu pra trás...'
-        else:
-            data['latestHate'] = latestHate
-            if latestHate >= 25000:
-                data['latestHateSubjective'] = u'Tá bom! Sou evangélico agora.'
-            elif latestHate >= 15000:
-                data['latestHateSubjective'] = u'OK OK! A terra é plana. Satifeitos?'
-            elif latestHate >= 7000:
-                data['latestHateSubjective'] = u'Se segura que lá vem chumbo!'
-            elif latestHate >= 4500:
-                data['latestHateSubjective'] = u'Ai meu Senhor Jesus...'
-            elif latestHate >= 3000:
-                data['latestHateSubjective'] = u'O canal é meu, viu!!?'
-            elif latestHate >= 1000:
-                data['latestHateSubjective'] = u'Haters gonna hate...'
-            elif latestHate > 500:
-                data['latestHateSubjective'] = u'Acho que ouvi algum zunido...'
-            elif latestHate <= 500:
-                data['latestHateSubjective'] = u'Nem faz cócegas...'
-
-        latestSixQuality = []
-        for i in range(0, 6):
-            quality = data['latestLikes'][i] >= data['latestDislikes'][i]*3
-            latestSixQuality.append(quality)
-
-        data['latestSixQuality'] = latestSixQuality
-
-        return data
+        return processData(data)
     else:
         return None
 
